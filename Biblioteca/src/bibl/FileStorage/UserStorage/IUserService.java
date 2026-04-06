425c8cb2870e6ac1d3ad5c38fb7a1328e9410c7e
abigail huezo <abigailhuezoc@gmail.com>
2026-04-06 10:14:05 -0600

    Documentar -Abigail H.

--- Biblioteca/src/bibl/FileStorage/UserStorage/IUserService.java
+++ Biblioteca/src/bibl/FileStorage/UserStorage/IUserService.java
@@ -2,7 +2,10 @@ package bibl.FileStorage.UserStorage;
 
 import bibl.user.User;
 import java.util.List;
-
+/** Contrato para la gestión de usuarios
+ * Define las acciones esenciales para administrar a las personas que
+ * interactúan con la biblioteca
+ */
 public interface IUserService {
     void addUser(User user);
     void showUsers();
--- Biblioteca/src/bibl/FileStorage/UserStorage/UserService.java
+++ Biblioteca/src/bibl/FileStorage/UserStorage/UserService.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import bibl.user.User;
-
+/**Esta clase centraliza la base de datos de usuarios registrados
+ * Implementa IUserService para separar la definición de la lógica real
+ */
 public class UserService implements IUserService {
-
+    // Encapsulamiento: Lista privada para que el acceso sea controlado
+    // exclusivamente por los métodos de esta clase.
     private List<User> registered;
 
     public UserService() {
@@ -15,6 +18,7 @@ public class UserService implements IUserService {
 
     @Override
     public void addUser(User user) {
+        // Validación preventiva: No permitimos que entren nulos al sistema
         if (user != null) {
             this.registered.add(user);
             System.out.println("Persona agregada exitosamente: " + user.getName());
@@ -31,10 +35,13 @@ public class UserService implements IUserService {
         }
         for (User user : this.registered) {
             System.out.println("=====================================");
+            // Polimorfismo: Llamamos a showInformation() que viene de Person y se hereda en User
             user.showInformation();
         }
     }
-
+    /** Buscador por ID.
+     * El ID es nuestra "llave primaria", por lo que la búsqueda es exacta
+     */
     @Override
     public User findUserById(String searchId) {
         if (searchId == null || searchId.trim().isEmpty()) {
@@ -47,9 +54,13 @@ public class UserService implements IUserService {
                 return user;
             }
         }
+        // Si no lo encontramos, lanzamos una excepción clara para que el Main sepa qué pasó
         throw new java.util.NoSuchElementException("No se encontró ningún usuario con el ID: " + searchId);
     }
-
+    /**Seguridad de la info
+     * Retornamos una vista de "solo lectura" de la lista.
+     * Esto evita que se puedan añadir o quitar usuarios externamente sin pasar por addUser()
+     */
     @Override
     public List<User> getRegistered() {
         return Collections.unmodifiableList(registered);
